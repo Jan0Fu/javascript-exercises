9f5eab330a8d165b076d58cc8a7198b6f738c87c
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-10-27 12:20:32 +0200

    Completed all fundamental js exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
-};
-
+const sumAll = (min, max) => {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  return ((max-min) + 1) * (min + max) / 2;
+} 
 // Do not edit below this line
 module.exports = sumAll;
