8fc5bbe4d67c29b1eefd19bbd9180fa1130cbf43
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-10-30 22:00:55 +0100

    Nailed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = 0;
+  for (let index = 0; index < arr.length; index++) {
+    result += arr[index];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = arr[0];//or 1 but after that let index = 0
+  for (let index = 1; index < arr.length; index++) {
+    result *= arr[index];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let result = a;
+  for (let i = 1; i < b; i++) {
+    result *= a;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
