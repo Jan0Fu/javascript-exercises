401b21f372553a89d61c077b2521da8770523f59
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-10-31 09:13:00 +0100

    Completed palindromes and fibonacii exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function(str) {
+    processedStr = str.toLowerCase().replace(/[^a-z]/g, "");
+    return (processedStr.split("").reverse().join("") == processedStr);
+  };
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = (n) => {
+    let a = 0, b = 1, c = n;
+    for (let i = 2; i <= n; i++ ) {
+    c = a + b;
+    a = b;
+    b = c;
+  }
+    if (n > 0) {
+        return parseInt(c);
+    }
+    return "OOPS"
 };
 
 // Do not edit below this line
