ef37a4106322f39c870637247f38cf6bf64d027c
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2022-10-31 13:20:08 +0100

    Fixed some comments in exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,11 +1,12 @@
+//using reduce method to find oldest person, comparing current oldest to all persons in array
 const findTheOldest = function(arr) {
-    return arr.reduce((oldest, currentPerson) => {
+    return arr.reduce((oldest, currentPerson) => {//comparing parameter oldest to current object(person)
       const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
       const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
       return oldestAge < currentAge ? currentPerson : oldest;
     })
 };
-
+// function to get persons age: year of death(or current year) - year of birth
 const getAge = function(birth, death) {
     if (!death) {
       death = new Date().getFullYear();
